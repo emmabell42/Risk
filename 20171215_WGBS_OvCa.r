efb00315022157c069f2e75b8cace75f5e4352f9
Emma Bell <emmabell42@gmail.com>
2018-01-11 12:20:03 +0000

    Incorporate BrCa data
    Re-written to read in either or both the OvCa and BrCa data.

--- 20171215_WGBS_OvCa.r
+++ 20171215_WGBS_OvCa.r
@@ -1,15 +1,20 @@
-# This code reads the ovarian cancer WGBS into R
+# This code reads into R the WGBS CI data
 # It then looks for windows where the confidence intervals between cases and controls don't overlap by X amount
 # This code is adapted from 20171215_WGBS_BrCa_JF.r
 # The RDS files for each chromosome are stored here: /data/SHARE/GINA/methpredict/
+
+# Define the cancer type you want to read in as "OvCa", "BrCa", or c("OvCa","BrCa") if you want both data sets
+cancerType <- "BrCa"
 
 # Set a minimum difference for the control and case CIs
 min.diff <- 0
 gr.all <- c()
 
 # Read in the rds for each chromosome and concatenate into a single list of GRanges objects
-for (i in c(1:22,"X")){
-	filez<-paste("//data/SHARE/GINA/methpredict/","WGBS_OvCa_200bpWindowCIs_chr",i,".rds", sep="")
+for(i in 1:length(cancerType)){
+type <- cancerType[i]
+for (j in c(1:22,"X")){
+	filez<-paste("//data/SHARE/GINA/methpredict/WGBS_",type,"_200bpWindowCIs_chr",j,".rds", sep="")
 	cat("\n","Reading ",filez)
 	system.time({
 	wg.wgbs.windows<-readRDS(filez)
@@ -19,11 +24,19 @@ for (i in c(1:22,"X")){
 	gr.all.diff<-wg.wgbs.windows[c(wh1,wh2),]
 	gr.all<-c(gr.all,gr.all.diff)
 }
+toName <- paste0("gr.",type)
 names(gr.all) <- paste("chr",c(1:22,"X"),sep="")
+assign(toName,gr.all)
+}
 rm(min.diff)
 rm(i)
 rm(filez)
 rm(wg.wgbs.windows)
 rm(wh1)
 rm(wh2)
+rm(gr.all)
+rm(cancerType)
+rm(type)
 rm(gr.all.diff)
+rm(toName)
+rm(j)
