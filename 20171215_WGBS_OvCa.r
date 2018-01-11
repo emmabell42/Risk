e5c8aecfe6c6347028a6caf4d9da517e97ca29e2
Emma Bell <emmabell42@gmail.com>
2018-01-11 12:56:01 +0000

    Minor edits
    To keep the cancerType and min.diff objects

--- 20171215_WGBS_OvCa.r
+++ 20171215_WGBS_OvCa.r
@@ -12,30 +12,36 @@ gr.all <- c()
 
 # Read in the rds for each chromosome and concatenate into a single list of GRanges objects
 for(i in 1:length(cancerType)){
-type <- cancerType[i]
-for (j in c(1:22,"X")){
-	filez<-paste("//data/SHARE/GINA/methpredict/WGBS_",type,"_200bpWindowCIs_chr",j,".rds", sep="")
-	cat("\n","Reading ",filez)
-	system.time({
-	wg.wgbs.windows<-readRDS(filez)
-	})
-	wh1<-which(wg.wgbs.windows$ControlLCI-wg.wgbs.windows$CaseUCI>min.diff)
-	wh2<-which(wg.wgbs.windows$CaseLCI-wg.wgbs.windows$ControlUCI>min.diff)
-	gr.all.diff<-wg.wgbs.windows[c(wh1,wh2),]
-	gr.all<-c(gr.all,gr.all.diff)
+
+	type <- cancerType[i]
+
+	for (j in c(1:22,"X")){
+		filez<-paste("//data/SHARE/GINA/methpredict/WGBS_",type,"_200bpWindowCIs_chr",j,".rds", sep="")
+		cat("\n","Reading ",filez)
+		
+		system.time({
+			wg.wgbs.windows<-readRDS(filez)
+		})
+		
+		wh1<-which(wg.wgbs.windows$ControlLCI-wg.wgbs.windows$CaseUCI>min.diff)
+		wh2<-which(wg.wgbs.windows$CaseLCI-wg.wgbs.windows$ControlUCI>min.diff)
+		gr.all.diff<-wg.wgbs.windows[c(wh1,wh2),]
+		gr.all<-c(gr.all,gr.all.diff)
+
 }
+
 toName <- paste0("gr.",type)
 names(gr.all) <- paste("chr",c(1:22,"X"),sep="")
 assign(toName,gr.all)
+
 }
-rm(min.diff)
+
 rm(i)
 rm(filez)
 rm(wg.wgbs.windows)
 rm(wh1)
 rm(wh2)
 rm(gr.all)
-rm(cancerType)
 rm(type)
 rm(gr.all.diff)
 rm(toName)
